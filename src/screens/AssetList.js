5a8edc9ee2fbb18d59c23464863013b99b661459
Julian Bonomini <julian.bonomini@gmail.com>
2018-09-18 18:04:06 -0300

    coin item compoenet styled

--- src/screens/AssetList.js
+++ src/screens/AssetList.js
@@ -8,6 +8,7 @@ import {
 import { FloatingActionButton, AssetItem, Header } from '../components';
 import DataStorage from '../data/DataStorage';
 import { colors } from '../utils';
+import coinsLogos from '../assets';
 
 const styles = StyleSheet.create({
   container: {
@@ -27,15 +28,22 @@ const styles = StyleSheet.create({
     color: colors.WHITE,
     alignSelf: 'center',
   },
+  listContentContainer: {
+    width: '95%',
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginTop: 30,
+  },
 });
 
 export default class AssetList extends React.Component {
   static navigationOptions = {
-    header:
+    header: (
       <Header title="CryptoFolio">
         <Text style={styles.totalAmount}>$ 1,234.00</Text>
         <Text style={styles.totalAmountText}>Total Amount</Text>
-      </Header>,
+      </Header>
+    ),
   };
 
   constructor(props) {
@@ -46,15 +54,26 @@ export default class AssetList extends React.Component {
     };
   }
 
-  componentWillMount() {
+  componentDidMount() {
     // load assets from storage
-    DataStorage.getAssets().then((assets) => {
-      // add the assets to the state
-      this.setState(prevState => ({
-        ...prevState,
-        assets,
-      }));
-    });
+    // DataStorage.getAssets().then((assets) => {
+    //   // add the assets to the state
+    //   this.setState(prevState => ({
+    //     ...prevState,
+    //     assets,
+    //   }));
+    // });
+    const assets = [
+      {
+        coin: {
+          name: 'Bicoin',
+          ticker: 1,
+          logo: coinsLogos.btc,
+        },
+        amount: 1000,
+      },
+    ];
+    this.setState({ assets });
   }
 
   onPressItem = (item) => {
@@ -69,6 +88,7 @@ export default class AssetList extends React.Component {
       <View style={styles.container}>
         <FlatList
           style={styles.list}
+          contentContainerStyle={styles.listContentContainer}
           data={Object.values(this.state.assets)}
           keyExtractor={item => item.coin.ticker}
           renderItem={({ item }) => <AssetItem asset={item} onPressItem={this.onPressItem} />}
