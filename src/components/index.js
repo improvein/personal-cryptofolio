29e26cae114cc6220aa266075976ff6740b9ce9f
Julian Bonomini <julian.bonomini@gmail.com>
2018-09-19 00:26:58 -0300

    add coin button component created

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as FloatingActionButton } from './FloatingActionButton';
 export { default as AssetItem } from './AssetItem';
 export { default as Header } from './Header';
+export { default as AddCoinButton } from './AddCoinButton';
