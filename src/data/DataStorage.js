c3642e782ae51a9e26de47dfb8e241a1450e7c16
diegogurpegui <diego.gurpegui@improvein.com>
2018-11-03 20:25:20 -0300

    Coins data in a separate file.

--- src/data/DataStorage.js
+++ src/data/DataStorage.js
@@ -1,5 +1,5 @@
 import { AsyncStorage } from 'react-native';
-import coinsLogos from '../assets';
+import coins from './coins';
 
 const DATA_ASSETS = '@Data:assets';
 const DATA_ASSET_HIST = '@Data:assethist_';
@@ -27,65 +27,7 @@ class DataStorage {
    * Hard-coded data.
    * @returns List of assets
    */
-  static getCoins = async () => {
-    const coins = [
-      {
-        ticker: 'BTC',
-        name: 'Bitcoin',
-        logo: coinsLogos.btc,
-        availablePriceSources: ['binance', 'bitfinex', 'bitstamp'],
-      },
-      {
-        ticker: 'BCH',
-        name: 'Bitcoin Cash',
-        logo: coinsLogos.bch,
-        availablePriceSources: ['bitfinex', 'bitstamp'],
-      },
-      {
-        ticker: 'ADA',
-        name: 'Cardano',
-        logo: coinsLogos.ada,
-        availablePriceSources: ['binance'],
-      },
-      {
-        ticker: 'DASH',
-        name: 'Dash',
-        logo: coinsLogos.dash,
-        availablePriceSources: ['bitfinex'],
-      },
-      {
-        ticker: 'EOS',
-        name: 'EOS',
-        logo: coinsLogos.eos,
-        availablePriceSources: ['binance', 'bitfinex'],
-      },
-      {
-        ticker: 'ETH',
-        name: 'Ethereum',
-        logo: coinsLogos.eth,
-        availablePriceSources: ['binance', 'bitfinex', 'bitstamp'],
-      },
-      {
-        ticker: 'IOTA',
-        name: 'IOTA',
-        logo: coinsLogos.iota,
-        availablePriceSources: ['binance', 'bitfinex'],
-      },
-      {
-        ticker: 'LTC',
-        name: 'Litecoin',
-        logo: coinsLogos.ltc,
-        availablePriceSources: ['binance', 'bitfinex', 'bitstamp'],
-      },
-      {
-        ticker: 'XMR',
-        name: 'Monero',
-        logo: coinsLogos.xmr,
-        availablePriceSources: ['bitfinex'],
-      },
-    ];
-    return coins;
-  };
+  static getCoins = async () => coins;
 
   /**
    * Get the assets in the portfolio
