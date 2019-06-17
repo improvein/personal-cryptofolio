b9f71041c4cec9d504a9d94ce5983e60a24a78c0
Diego H. Gurpegui <diego.gurpegui@improvein.com>
2019-06-17 18:44:32 -0300

    Change AsyncStorage lib. Several fixes.

--- android/app/src/main/java/com/improvein/personalcryptofolio/MainApplication.java
+++ android/app/src/main/java/com/improvein/personalcryptofolio/MainApplication.java
@@ -3,6 +3,7 @@ package com.improvein.personalcryptofolio;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
             new RNDeviceInfo(),
             new MainReactPackage(),
+            new AsyncStoragePackage(),
             new LinearGradientPackage(),
             new RNGestureHandlerPackage()
       );
--- src/data/DataStorage.js
+++ src/data/DataStorage.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import sha256 from 'crypto-js/sha256';
 import coins from './coins';
 
